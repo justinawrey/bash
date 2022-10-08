7b8f3a64238b5fe76febf6f50897f78100e9f79c
Justin Awrey <awreyjustin@gmail.com>
2022-10-08 14:28:55 -0700

    feat: strip trailing newline from command output by default

--- mod.ts
+++ mod.ts
@@ -2,7 +2,16 @@ import { bold } from "https://deno.land/std@0.159.0/fmt/colors.ts";
 
 export class BashError extends Error {}
 
-export async function bash(cmd: string): Promise<string> {
+export interface BashOptions {
+  stripTrailingNewline: boolean;
+}
+
+export async function bash(
+  cmd: string,
+  options: BashOptions = {
+    stripTrailingNewline: true,
+  },
+): Promise<string> {
   const process = Deno.run({
     cmd: ["bash", "-c", cmd],
     stdout: "piped",
@@ -15,12 +24,19 @@ export async function bash(cmd: string): Promise<string> {
     process.stderrOutput(),
   ]);
 
+  // Success!
   if (status.code === 0) {
-    return new TextDecoder().decode(stdout);
-  } else {
-    throw new BashError(
-      `bash invocation ${bold(`'${cmd}'`)} failed with error:\n\n` +
-        new TextDecoder().decode(stderr),
-    );
+    const result = new TextDecoder().decode(stdout);
+    if (options.stripTrailingNewline) {
+      return result.replace(/\n$/, "");
+    }
+
+    return result;
   }
+
+  // Failure!
+  throw new BashError(
+    `bash invocation ${bold(`'${cmd}'`)} failed with error:\n\n` +
+      new TextDecoder().decode(stderr),
+  );
 }
